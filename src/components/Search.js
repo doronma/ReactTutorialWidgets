80ce75b14771efd402df6e6347a34dbc0d073ee7
Doron Marcus <doron.marcus@gmail.com>
2020-10-08 23:44:15 +0300

    stage1

---
+++ src/components/Search.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+
+const Search = () => {
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+
+    useEffect(() => {
+        /** useEffect is invoked firttime and when term changes
+           every time term is changed.
+           useEffect cannot use async functions directly
+        */
+        const search = async () => {
+            // extract data from result
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search);
+        };
+        if (term) {
+            search();
+        }
+
+        // axios.get('ab')
+        //     .then((response) => {
+        //         console.log(response.data);
+        //     })
+    }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`http://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    {/* XSS secutiry issue */}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
+
+                </div>
+            </div>
+        )
+    });
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input" />
+
+
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+        </div>
+    )
+}
+
+export default Search
